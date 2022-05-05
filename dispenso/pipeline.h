cbe1314855c96f911bbd664a02382f0adc2158b1
Kosie van der Merwe <kosievdmerwe@fb.com>
2022-05-05 15:36:11 -0700

    **][oss-safe] Meta license header migration
    Reviewed By: graphicsMan
    
    Differential Revision: D36157240
    
    fbshipit-source-id: ade005b3a5f56097adf8948e7644c8b7b15e6093

--- dispenso/pipeline.h
+++ dispenso/pipeline.h
@@ -1,7 +1,9 @@
-// Copyright (c) Facebook, Inc. and its affiliates.
-//
-// This source code is licensed under the MIT license found in the
-// LICENSE.md file in the root directory of this source tree.
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 /**
  * @file pipeline.h
