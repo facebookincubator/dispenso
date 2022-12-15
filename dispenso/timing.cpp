4f4745a4b452d406f15a5acc532a8a7d772d0b63
Brian Budge <bbudge@meta.com>
2022-12-14 21:42:22 -0800

    Attempt to make cpu-counter based timing more accurate
    Summary: Accuracy of counter-based timing had an error of about 1 part in 5000, which isn't super bad, but isn't super great.  With these changes, we achieve error of about 1 in 100K.  Additionally, previously the first call to getTime() would take 10ms.  I've changed this to happen before main(). The downside is that startup will now take 50ms; I think 1 20th of a second is reasonable to gain fast and accurate timer capability.  For Mac this can be avoided, since we can query the system for the TSC frequency.
    
    Reviewed By: EscapeZero
    
    Differential Revision:
    D41775291
    
    Privacy Context Container: L1124100
    
    fbshipit-source-id: 39a5c21f1698b917bfe6ab4841f2f5b61c117d5a

--- dispenso/timing.cpp
+++ dispenso/timing.cpp
@@ -8,10 +8,20 @@
 #include <dispenso/timing.h>
 
 #include <chrono>
+#include <cmath>
 
 #if defined(_MSC_VER)
 #include <intrin.h>
 #endif // _MSC_VER
+
+#if defined(_WIN32)
+#include <Windows.h>
+#endif // _WIN32
+
+#if defined(__MACH__)
+#include <mach/mach.h>
+#include <mach/mach_time.h>
+#endif // __MACH__
 
 namespace dispenso {
 namespace {
@@ -22,6 +32,7 @@ inline uint64_t rdtscp() {
   uint32_t ui;
   return __rdtscp(&ui);
 }
+
 #else
 inline uint64_t rdtscp() {
   uint32_t lo, hi;
@@ -44,28 +55,97 @@ uint64_t rdtscp(void) {
 
 #if defined(DISPENSO_HAS_TIMESTAMP)
 
-std::pair<double, double> initTime() {
+bool snapFreq(double& firstApprox) {
+  switch ((int)firstApprox) {
+    case 0:
+      if (std::abs(int(firstApprox * 10.0)) <= 1) {
+        firstApprox = 0.0;
+        return true;
+      }
+      break;
+    case 9:
+      if (std::abs(int(firstApprox * 10.0) - 99) <= 1) {
+        firstApprox = 10.0;
+
+        return true;
+      }
+      break;
+    case 3:
+      if (std::abs(int(firstApprox * 10.0) - 33) <= 1) {
+        firstApprox = 3.0 + 1.0 / 3.0;
+        return true;
+      }
+      break;
+    case 6:
+      if (std::abs(int(firstApprox * 10.0) - 66) <= 1) {
+        firstApprox = 6.0 + 2.0 / 3.0;
+        return true;
+      }
+      break;
+  }
+  return false;
+}
+
+double fallbackTicksPerSecond() {
   using namespace std::chrono_literals;
+  constexpr double kChronoOverheadBias = 250e-9;
 
   auto baseStart = std::chrono::high_resolution_clock::now();
   auto start = rdtscp();
-  std::this_thread::sleep_for(10ms);
+  std::this_thread::sleep_for(50ms);
   auto end = rdtscp();
   auto baseEnd = std::chrono::high_resolution_clock::now();
 
-  auto base = std::chrono::duration<double>(baseEnd - baseStart).count();
-  double secondsPerCycle = base / static_cast<double>(end - start);
+  auto base = std::chrono::duration<double>(baseEnd - baseStart).count() - kChronoOverheadBias;
+  double firstApprox = (static_cast<double>(end - start)) / base;
 
-  double startTime = static_cast<double>(start) * secondsPerCycle;
-  return {secondsPerCycle, startTime};
+  // Try to refine the approximation.  In some circumstances we can "snap" the frequency to a very
+  // good guess that is off by less than one part in thousands.  Accuracy should already be quite
+  // good in any case, but this allows us to improve in some cases.
+
+  // Get first 3 digits
+  firstApprox *= 1e-7;
+
+  int firstInt = (int)firstApprox;
+  firstApprox -= firstInt;
+
+  firstApprox *= 10.0;
+
+  if (!snapFreq(firstApprox)) {
+    int secondInt = (int)firstApprox;
+    firstApprox -= secondInt;
+    firstApprox *= 10.0;
+    snapFreq(firstApprox);
+    firstApprox *= 0.1;
+    firstApprox += secondInt;
+  }
+
+  firstApprox *= 0.1;
+
+  firstApprox += firstInt;
+  firstApprox *= 1e7;
+  return firstApprox;
 }
 
-double getTime() {
-  static std::pair<double, double> tmInfo = initTime();
-  double& secondsPerCycle = tmInfo.first;
-  double& startTime = tmInfo.second;
+#if defined(__MACH__)
+double ticksPerSecond() {
+  mach_timebase_info_data_t info;
+  if (mach_timebase_info(&info) != KERN_SUCCESS) {
+    return fallbackTicksPerSecond();
+  }
+  return 1e9 * static_cast<double>(info.denom) / static_cast<double>(info.numer);
+}
+#else
+double ticksPerSecond() {
+  return fallbackTicksPerSecond();
+}
+#endif
 
-  double t = static_cast<double>(rdtscp()) * secondsPerCycle;
+double getTime() {
+  static double secondsPerTick = 1.0 / ticksPerSecond();
+  static double startTime = static_cast<double>(rdtscp()) * secondsPerTick;
+
+  double t = static_cast<double>(rdtscp()) * secondsPerTick;
   return t - startTime;
 }
 #else
@@ -76,5 +156,10 @@ double getTime() {
   return std::chrono::duration<double>(cur - startTime).count();
 }
 #endif // DISPENSO_HAS_TIMESTAMP
+
+namespace {
+// This should ensure that we initialize the time before main.
+double g_dummyTime = getTime();
+} // namespace
 
 } // namespace dispenso
